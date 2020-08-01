b159f48351bb5189b5d610d69a202abb1d463c82
byom26 <p.byom26@gmail.com>
2020-08-02 00:12:00 +0530

    lesson1

---
+++ first.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main() {
+    printf("Hello, I am learning C with Harry");
+    return 0;
+}
+
+
+// Single line comment
+/* Multi
+   Line 
+   Comment */
---
+++ variables.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main() {
+    char x = 'a';
+    int y = 10;
+    float z = 10.5;
+
+    printf("Value of x is %c \n", x);
+    printf("Value of y is %d \n", y);
+    printf("Value of z is %f \n", z);
+
+    return 0;
+}
